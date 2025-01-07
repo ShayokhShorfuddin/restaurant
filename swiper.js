83016ea557e7139bf8656ef3a73f95c764aea343
Shayokh <shayokhshuvro14@gmail.com>
2025-01-07 18:25:50 +0600

    add assets, implement featured restaurant section

--- swiper.js
+++ swiper.js
@@ -1,5 +1,16 @@
-const swiper = new Swiper(".swiper", {
+// Swiper for offers
+const offersSwiper = new Swiper(".offers-swiper", {
 	slidesPerView: 3,
+	spaceBetween: 25,
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+});
+
+// Swiper for featured restaurants
+const featuredRestaurantSwiper = new Swiper(".featured-restaurants-swiper", {
+	slidesPerView: 4,
 	spaceBetween: 25,
 	navigation: {
 		nextEl: ".swiper-button-next",
