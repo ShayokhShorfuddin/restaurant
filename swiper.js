4377997bdc3cbc1dcc3ae97de7bf8eaf5e2052c1
Shayokh <shayokhshuvro14@gmail.com>
2025-01-08 17:22:15 +0600

    add best selling restaurants and new restaurants section

--- swiper.js
+++ swiper.js
@@ -37,3 +37,26 @@ const cuisinesSwiper = new Swiper(".cuisines-swiper", {
 		prevEl: ".swiper-button-prev",
 	},
 });
+
+// Swiper for best selling restaurants
+const bestSellingRestaurantsSwiper = new Swiper(
+	".best-selling-restaurants-swiper",
+	{
+		slidesPerView: 4,
+		spaceBetween: 25,
+		navigation: {
+			nextEl: ".swiper-button-next",
+			prevEl: ".swiper-button-prev",
+		},
+	},
+);
+
+// Swiper for new restaurants
+const newRestaurantsSwiper = new Swiper(".new-restaurants-swiper", {
+	slidesPerView: 4,
+	spaceBetween: 25,
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+});
