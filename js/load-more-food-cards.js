9f712d3c0966ee561afd78385cffcafd0085a9d5
Shayokh <shayokhshuvro14@gmail.com>
2025-02-04 15:49:52 +0600

    fix load-more image not found issue

--- js/load-more-food-cards.js
+++ js/load-more-food-cards.js
@@ -1,67 +1,72 @@
 import $ from "jquery";
+import meetAllMeatImage from './assets/images/meet-all-meat.png';
+import kebabIshImage from './assets/images/kebab-ish.png';
+import pizzaleciousImage from './assets/images/pizzalecious.png';
+import chicksawImage from './assets/images/chicksaw.png';
+import starImage from './assets/images/star.svg';
 
 const foodCards = `
         <!-- Food card 5 -->
         <div class="highlight-on-hover">
             <div class="restaurants-at-woodland-hills-swiper-img-holder rounded-4 glow-on-hover">
-                <img src="./assets/images/meet-all-meat.png" alt="meet-all-meat">
+                <img src="${meetAllMeatImage}" alt="meet-all-meat">
             </div>
             <h6 class="fs-15 fw-bold mt-3">Meet all Meat</h6>
             <p class="fs-14 text-body-secondary mt-1">Pizza, Pasta, Italian, Lasa….</p>
             <div class="d-flex mt-2 column-gap-2">
-                <img src="./assets/images/star.svg" alt="Star">
-                <img src="./assets/images/star.svg" alt="Star">
-                <img src="./assets/images/star.svg" alt="Star">
-                <img src="./assets/images/star.svg" alt="Star">
-                <img src="./assets/images/star.svg" alt="Star">
+                <img src="${starImage}" alt="Star">
+                <img src="${starImage}" alt="Star">
+                <img src="${starImage}" alt="Star">
+                <img src="${starImage}" alt="Star">
+                <img src="${starImage}" alt="Star">
             </div>
         </div>
 
         <!-- Food card 6 -->
         <div class="highlight-on-hover">
             <div class="restaurants-at-woodland-hills-swiper-img-holder rounded-4 glow-on-hover">
-                <img src="./assets/images/kebab-ish.png" alt="kebab-ish">
+                <img src="${kebabIshImage}" alt="kebab-ish">
             </div>
             <h6 class="fs-15 fw-bold mt-3">Kebab Ish</h6>
             <p class="fs-14 text-body-secondary mt-1">Pizza, Pasta, Italian, Lasa….</p>
             <div class="d-flex mt-2 column-gap-2">
-                <img src="./assets/images/star.svg" alt="Star">
-                <img src="./assets/images/star.svg" alt="Star">
-                <img src="./assets/images/star.svg" alt="Star">
-                <img src="./assets/images/star.svg" alt="Star">
-                <img src="./assets/images/star.svg" alt="Star">
+                <img src="${starImage}" alt="Star">
+                <img src="${starImage}" alt="Star">
+                <img src="${starImage}" alt="Star">
+                <img src="${starImage}" alt="Star">
+                <img src="${starImage}" alt="Star">
             </div>
         </div>
 
         <!-- Food card 7 -->
         <div class="highlight-on-hover">
             <div class="restaurants-at-woodland-hills-swiper-img-holder rounded-4 glow-on-hover">
-                <img src="./assets/images/pizzalecious.png" alt="Pizzalecious">
+                <img src="${pizzaleciousImage}" alt="Pizzalecious">
             </div>
             <h6 class="fs-15 fw-bold mt-3">Pizzalecious</h6>
             <p class="fs-14 text-body-secondary mt-1">Pizza, Pasta, Italian, Lasa….</p>
             <div class="d-flex mt-2 column-gap-2">
-                <img src="./assets/images/star.svg" alt="Star">
-                <img src="./assets/images/star.svg" alt="Star">
-                <img src="./assets/images/star.svg" alt="Star">
-                <img src="./assets/images/star.svg" alt="Star">
-                <img src="./assets/images/star.svg" alt="Star">
+                <img src="${starImage}" alt="Star">
+                <img src="${starImage}" alt="Star">
+                <img src="${starImage}" alt="Star">
+                <img src="${starImage}" alt="Star">
+                <img src="${starImage}" alt="Star">
             </div>
         </div>
 
         <!-- Food card 8 -->
         <div class="highlight-on-hover">
             <div class="restaurants-at-woodland-hills-swiper-img-holder rounded-4 glow-on-hover">
-                <img src="./assets/images/chicksaw.png" alt="Chicksaw Chickens">
+                <img src="${chicksawImage}" alt="Chicksaw Chickens">
             </div>
             <h6 class="fs-15 fw-bold mt-3">Chicksaw Chickens</h6>
             <p class="fs-14 text-body-secondary mt-1">Pizza, Pasta, Italian, Lasa….</p>
             <div class="d-flex mt-2 column-gap-2">
-                <img src="./assets/images/star.svg" alt="Star">
-                <img src="./assets/images/star.svg" alt="Star">
-                <img src="./assets/images/star.svg" alt="Star">
-                <img src="./assets/images/star.svg" alt="Star">
-                <img src="./assets/images/star.svg" alt="Star">
+                <img src="${starImage}" alt="Star">
+                <img src="${starImage}" alt="Star">
+                <img src="${starImage}" alt="Star">
+                <img src="${starImage}" alt="Star">
+                <img src="${starImage}" alt="Star">
             </div>
         </div>
 `;
